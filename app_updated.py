0060a7c26b5a53a2b696d868a3c32e196bf6c893
q8atnight <q8atnight@gmail.com>
2025-09-06 11:36:32 -0700

    Add app_updated.py decompress script

---
+++ app_updated.py
@@ -0,0 +1,15 @@
+import zipfile, os, sys
+
+ZIP_FILE = 'Rentals_V5_updated.zip'
+EXTRACT_DIR = 'deployed_app'
+
+# Extract the zip if not already done
+if not os.path.isdir(EXTRACT_DIR):
+    with zipfile.ZipFile(ZIP_FILE, 'r') as zip_ref:
+        zip_ref.extractall(EXTRACT_DIR)
+
+# Add extracted project path to sys.path
+sys.path.insert(0, os.path.join(EXTRACT_DIR, 'Rentals_V5'))
+
+# Import the Flask app from the extracted code
+from app import app
